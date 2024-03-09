ae7b8307094fb945307bf76e09c9731cacb9a723
ohyeahx1 <90593572+ohyeahx1@users.noreply.github.com>
2024-03-09 22:42:45 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// Helper function to add two numbers
+function adNs(n1, n2) {
+    return n1 + n2;
+  }
+  
+  // Helper function to square a number
+  function sqNs(number) {
+    return number * number;
+  }
+  
+  // Main function to perform the operation
+  function ad_sq(x, y) {
+    const sum = adNs(x, y);
+    const rs = sqNs(sum);
+    return rs;
+  }
+  
+  // Example of usage:
+  const num1 = 13;
+  const num2 = 15;
+  const rs = ad_sq(num1, num2);
+  
+  console.log(`(${num1} + ${num2})^2 = ${rs}`);
